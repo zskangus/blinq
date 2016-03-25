1c59f1e6f515a5b9adcddc9a94b2dedff33b3718
zsk <zsk@zskdemacbook-pro.local>
2016-03-25 09:33:18 +0800

    Initial Commit

---
+++ Blinq/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blinq
+//
+//  Created by zsk on 16/3/25.
+//  Copyright © 2016年 zsk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blinq/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blinq
+//
+//  Created by zsk on 16/3/25.
+//  Copyright © 2016年 zsk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
